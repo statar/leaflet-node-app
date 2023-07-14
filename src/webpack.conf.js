079867e80b33051bd5d4836f85bca8e65f61742e
Sevnur Tatar <sevnurtatar@gmail.com>
2023-07-15 01:19:29 +0200

    clean code

--- src/webpack.conf.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-    },
-};
